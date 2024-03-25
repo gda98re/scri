b5d90dca5580609abfa2a5841502ca7ca62def26
Guido Da Re <gdare@caltech.edu>
2024-03-24 18:24:41 -0700

    coped the modified version of transform + building the interpolant if not present + two flags

--- scri/asymptotic_bondi_data/transformations.py
+++ scri/asymptotic_bondi_data/transformations.py
@@ -197,88 +197,6 @@ def conformal_factors(boost_velocity, distorted_grid_rotors):
 
 
 def transform(self, **kwargs):
-    """Apply BMS transformation to AsymptoticBondiData object
-
-    It is important to note that the input transformation parameters are applied in this order:
-
-      1. (Super)Translations
-      2. Rotation (about the origin)
-      3. Boost (about the origin)
-
-    All input parameters refer to the transformation required to take the input data's inertial
-    frame onto the inertial frame of the output data's inertial observers.  In what follows, the
-    coordinates of and functions in the input inertial frame will be unprimed, while corresponding
-    values of the output inertial frame will be primed.
-
-    The translations (space, time, spacetime, or super) can be given in various ways, which may
-    override each other.  Ultimately, however, they are essentially combined into a single function
-    `α`, representing the supertranslation, which transforms the asymptotic time variable `u` as
-
-        u'(u, θ, ϕ) = u(u, θ, ϕ) - α(θ, ϕ)
-
-    A simple time translation by δt would correspond to
-
-        α(θ, ϕ) = δt  # Independent of (θ, ϕ)
-
-    A pure spatial translation δx would correspond to
-
-        α(θ, ϕ) = -δx · n̂(θ, ϕ)
-
-    where `·` is the usual dot product, and `n̂` is the unit vector in the given direction.
-
-
-    Parameters
-    ==========
-    abd: AsymptoticBondiData
-        The object storing the modes of the original data, which will be transformed in this
-        function.  This is the only required argument to this function.
-    time_translation: float, optional
-        Defaults to zero.  Nonzero overrides corresponding components of `spacetime_translation` and
-        `supertranslation` parameters.  Note that this is the actual change in the coordinate value,
-        rather than the corresponding mode weight (which is what `supertranslation` represents).
-    space_translation : float array of length 3, optional
-        Defaults to empty (no translation).  Non-empty overrides corresponding components of
-        `spacetime_translation` and `supertranslation` parameters.  Note that this is the actual
-        change in the coordinate value, rather than the corresponding mode weight (which is what
-        `supertranslation` represents).
-    spacetime_translation : float array of length 4, optional
-        Defaults to empty (no translation).  Non-empty overrides corresponding components of
-        `supertranslation`.  Note that this is the actual change in the coordinate value, rather
-        than the corresponding mode weight (which is what `supertranslation` represents).
-    supertranslation : complex array [defaults to 0]
-        This gives the complex components of the spherical-harmonic expansion of the
-        supertranslation in standard form, starting from ell=0 up to some ell_max, which may be
-        different from the ell_max of the input `abd` object.  Supertranslations must be real, so
-        these values should obey the condition
-            α^{ℓ,m} = (-1)^m ᾱ^{ℓ,-m}
-        This condition is actually imposed on the input data, so imaginary parts of α(θ, ϕ) will
-        essentially be discarded.  Defaults to empty, which causes no supertranslation.  Note that
-        some components may be overridden by the parameters above.
-    frame_rotation : quaternion [defaults to 1]
-        Transformation applied to (x,y,z) basis of the input mode's inertial frame.  For example,
-        the basis z vector of the new frame may be written as
-           z' = frame_rotation * z * frame_rotation.inverse()
-        Defaults to 1, corresponding to the identity transformation (no rotation).
-    boost_velocity : float array of length 3 [defaults to (0, 0, 0)]
-        This is the three-velocity vector of the new frame relative to the input frame.  The norm of
-        this vector is required to be smaller than 1.
-    output_ell_max: int [defaults to abd.ell_max]
-        Maximum ell value in the output data.
-    working_ell_max: int [defaults to 2 * abd.ell_max]
-        Maximum ell value to use during the intermediate calculations.  Rotations and time
-        translations do not require this to be any larger than abd.ell_max, but other
-        transformations will require more values of ell for accurate results.  In particular, boosts
-        are multiplied by time, meaning that a large boost of data with large values of time will
-        lead to very large power in higher modes.  Similarly, large (super)translations will couple
-        power through a lot of modes.  To avoid aliasing, this value should be large, to accomodate
-        power in higher modes.
-
-    Returns
-    -------
-    abdprime: AsymptoticBondiData
-        Object representing the transformed data.
-
-    """
     from quaternion import rotate_vectors
     from scipy.interpolate import CubicSpline
 
@@ -294,7 +212,17 @@ def transform(self, **kwargs):
     n_phi = n_theta
     β = np.linalg.norm(boost_velocity)
     γ = 1 / math.sqrt(1 - β**2)
-
+    print('mod')
+    
+    if self.interpolant == None:
+        grid_rotors = boosted_grid(np.quaternion(1, 0, 0, 0), np.zeros(3), n_theta, n_phi)
+        self.interpolant = CubicSpline(self.u, np.array([sf.Grid(self.psi0.evaluate(grid_rotors), spin_weight=2),
+                                               sf.Grid(self.psi1.evaluate(grid_rotors), spin_weight=1),
+                                               sf.Grid(self.psi2.evaluate(grid_rotors), spin_weight=0),
+                                               sf.Grid(self.psi3.evaluate(grid_rotors), spin_weight=-1),
+                                               sf.Grid(self.psi4.evaluate(grid_rotors), spin_weight=-2),
+                                               sf.Grid(self.sigma.evaluate(grid_rotors), spin_weight=2)]), axis=1)
+    
     # Make this into a Modes object, so it can keep track of its spin weight, etc., through the
     # various operations needed below.
     supertranslation = sf.Modes(supertranslation, spin_weight=0).real
@@ -305,7 +233,7 @@ def transform(self, **kwargs):
     # original time slices.
     distorted_grid_rotors = boosted_grid(frame_rotation, boost_velocity, n_theta, n_phi)
 
-    # Compute u, α, ðα, ððα, k, ðk/k, 1/k, and 1/k³ on the distorted grid, including new axes to
+    # Compute u, α, ðα, ððα, k, ðk/k, 1/k, and 1/k on the distorted grid, including new axes to
     # enable broadcasting with time-dependent functions.  Note that the first axis should represent
     # variation in u, the second axis variation in θ', and the third axis variation in ϕ'.
     u = self.u
@@ -316,28 +244,38 @@ def transform(self, **kwargs):
     ]
     ððα = sf.Grid(0.5 * supertranslation.eth.eth.evaluate(distorted_grid_rotors), spin_weight=α.s + 2)[np.newaxis, :, :]
     k, ðk_over_k, one_over_k, one_over_k_cubed = conformal_factors(boost_velocity, distorted_grid_rotors)
-
-    # ðu'(u, θ', ϕ') exp(iλ) / k(θ', ϕ')
-    ðuprime_over_k = ðk_over_k * (u - α) - ðα
-
-    # ψ0(u, θ', ϕ') exp(2iλ)
-    ψ0 = sf.Grid(self.psi0.evaluate(distorted_grid_rotors), spin_weight=2)
-    # ψ1(u, θ', ϕ') exp(iλ)
-    ψ1 = sf.Grid(self.psi1.evaluate(distorted_grid_rotors), spin_weight=1)
+    
+    timeprime = (u - sf.constant_from_ell_0_mode(supertranslation[0]).real) / γ
+    timeprime_of_initialtime_directionprime = k * (u[0] - α)
+    timeprime_of_finaltime_directionprime = k * (u[-1] - α)
+    earliest_complete_timeprime = np.max(timeprime_of_initialtime_directionprime.view(np.ndarray))
+    latest_complete_timeprime = np.min(timeprime_of_finaltime_directionprime.view(np.ndarray))
+    timeprime = timeprime[(timeprime >= earliest_complete_timeprime) & (timeprime <= latest_complete_timeprime)]
+    
+    # This will store the values of f(u', θ, ϕ) for the various functions `f`
+    f_of_timeprime = self.interpolant(timeprime)
+                
+    # ðu'(u', θ', ϕ') exp(iλ) / k(θ', ϕ')
+    ðuprime_over_k = -sf.Grid(CubicSpline(u, -one_over_k * ðk_over_k * u + ðα)(timeprime), spin_weight=1)
+    
+    # ψ0(u', θ', ϕ') exp(2iλ)
+    ψ0 = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[0], 2, working_ell_max), spin_weight=2).evaluate(distorted_grid_rotors), spin_weight=2)
+    # ψ1(u', θ', ϕ') exp(iλ)
+    ψ1 = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[1], 1, working_ell_max), spin_weight=1).evaluate(distorted_grid_rotors), spin_weight=1)
     # ψ2(u, θ', ϕ')
-    ψ2 = sf.Grid(self.psi2.evaluate(distorted_grid_rotors), spin_weight=0)
-    # ψ3(u, θ', ϕ') exp(-1iλ)
-    ψ3 = sf.Grid(self.psi3.evaluate(distorted_grid_rotors), spin_weight=-1)
-    # ψ4(u, θ', ϕ') exp(-2iλ)
-    ψ4 = sf.Grid(self.psi4.evaluate(distorted_grid_rotors), spin_weight=-2)
-    # σ(u, θ', ϕ') exp(2iλ)
-    σ = sf.Grid(self.sigma.evaluate(distorted_grid_rotors), spin_weight=2)
-
+    ψ2 = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[2], 0, working_ell_max), spin_weight=0).evaluate(distorted_grid_rotors), spin_weight=0)
+    # ψ3(u', θ', ϕ') exp(-1iλ)
+    ψ3 = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[3], -1, working_ell_max), spin_weight=-1).evaluate(distorted_grid_rotors), spin_weight=-1)
+    # ψ4(u', θ', ϕ') exp(-2iλ)
+    ψ4 = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[4], -2, working_ell_max), spin_weight=-2).evaluate(distorted_grid_rotors), spin_weight=-2)
+    # σ(u', θ', ϕ') exp(2iλ)
+    σ = sf.Grid(sf.Modes(spinsfast.map2salm(f_of_timeprime[5], 2, working_ell_max), spin_weight=2).evaluate(distorted_grid_rotors), spin_weight=2)
+    
     ### The following calculations are done using in-place Horner form.  I suspect this will be the
     ### most efficient form of this calculation, within reason.  Note that the factors of exp(isλ)
     ### were computed automatically by evaluating in terms of quaternions.
     #
-    fprime_of_timenaught_directionprime = np.empty((6, self.n_times, n_theta, n_phi), dtype=complex)
+    fprime_of_timeprime_directionprime = np.empty((6, timeprime.size, n_theta, n_phi), dtype=complex)
     # ψ0'(u, θ', ϕ')
     fprime_temp = ψ4.copy()
     fprime_temp *= ðuprime_over_k
@@ -349,7 +287,7 @@ def transform(self, **kwargs):
     fprime_temp *= ðuprime_over_k
     fprime_temp += ψ0
     fprime_temp *= one_over_k_cubed
-    fprime_of_timenaught_directionprime[0] = fprime_temp
+    fprime_of_timeprime_directionprime[0] = fprime_temp
     # ψ1'(u, θ', ϕ')
     fprime_temp = -ψ4
     fprime_temp *= ðuprime_over_k
@@ -359,7 +297,7 @@ def transform(self, **kwargs):
     fprime_temp *= ðuprime_over_k
     fprime_temp += ψ1
     fprime_temp *= one_over_k_cubed
-    fprime_of_timenaught_directionprime[1] = fprime_temp
+    fprime_of_timeprime_directionprime[1] = fprime_temp
     # ψ2'(u, θ', ϕ')
     fprime_temp = ψ4.copy()
     fprime_temp *= ðuprime_over_k
@@ -367,53 +305,26 @@ def transform(self, **kwargs):
     fprime_temp *= ðuprime_over_k
     fprime_temp += ψ2
     fprime_temp *= one_over_k_cubed
-    fprime_of_timenaught_directionprime[2] = fprime_temp
+    fprime_of_timeprime_directionprime[2] = fprime_temp
     # ψ3'(u, θ', ϕ')
     fprime_temp = -ψ4
     fprime_temp *= ðuprime_over_k
     fprime_temp += ψ3
     fprime_temp *= one_over_k_cubed
-    fprime_of_timenaught_directionprime[3] = fprime_temp
+    fprime_of_timeprime_directionprime[3] = fprime_temp
     # ψ4'(u, θ', ϕ')
     fprime_temp = ψ4.copy()
     fprime_temp *= one_over_k_cubed
-    fprime_of_timenaught_directionprime[4] = fprime_temp
+    fprime_of_timeprime_directionprime[4] = fprime_temp
     # σ'(u, θ', ϕ')
     fprime_temp = σ.copy()
     fprime_temp -= ððα
     fprime_temp *= one_over_k
-    fprime_of_timenaught_directionprime[5] = fprime_temp
-
-    # Determine the new time slices.  The set timeprime is chosen so that on each slice of constant
-    # u'_i, the average value of u=(u'/k)+α is precisely <u>=u'γ+<α>=u_i.  But then, we have to
-    # narrow that set down, so that every grid point on all the u'_i' slices correspond to data in
-    # the range of input data.
-    timeprime = (u - sf.constant_from_ell_0_mode(supertranslation[0]).real) / γ
-    timeprime_of_initialtime_directionprime = k * (u[0] - α)
-    timeprime_of_finaltime_directionprime = k * (u[-1] - α)
-    earliest_complete_timeprime = np.max(timeprime_of_initialtime_directionprime.view(np.ndarray))
-    latest_complete_timeprime = np.min(timeprime_of_finaltime_directionprime.view(np.ndarray))
-    timeprime = timeprime[(timeprime >= earliest_complete_timeprime) & (timeprime <= latest_complete_timeprime)]
-
-    # This will store the values of f'(u', θ', ϕ') for the various functions `f`
-    fprime_of_timeprime_directionprime = np.zeros((6, timeprime.size, n_theta, n_phi), dtype=complex)
-
-    # Interpolate the various transformed function values on the transformed grid from the original
-    # time coordinate to the new set of time coordinates, independently for each direction.
-    for i in range(n_theta):
-        for j in range(n_phi):
-            k_i_j = k[0, i, j]
-            α_i_j = α[0, i, j]
-            # u'(u, θ', ϕ')
-            timeprime_of_timenaught_directionprime_i_j = k_i_j * (u - α_i_j)
-            # f'(u', θ', ϕ')
-            fprime_of_timeprime_directionprime[:, :, i, j] = CubicSpline(
-                timeprime_of_timenaught_directionprime_i_j, fprime_of_timenaught_directionprime[:, :, i, j], axis=1
-            )(timeprime)
+    fprime_of_timeprime_directionprime[5] = fprime_temp
 
     # Finally, transform back from the distorted grid to the SWSH mode weights as measured in that
     # grid.  I'll abuse notation slightly here by indicating those "distorted" mode weights with
-    # primes, so that f'(u')_{ℓ', m'} = ∫ f'(u', θ', ϕ') sȲ_{ℓ', m'}(θ', ϕ') sin(θ') dθ' dϕ'
+    # primes, so that f'(u')_{ℓ', m'} =  f'(u', θ', ϕ') sȲ_{ℓ', m'}(θ', ϕ') sin(θ') dθ' dϕ'
     abdprime = type(self)(timeprime, output_ell_max)
     # ψ0'(u')_{ℓ', m'}
     abdprime.psi0 = spinsfast.map2salm(fprime_of_timeprime_directionprime[0], 2, output_ell_max)
@@ -427,5 +338,7 @@ def transform(self, **kwargs):
     abdprime.psi4 = spinsfast.map2salm(fprime_of_timeprime_directionprime[4], -2, output_ell_max)
     # σ'(u')_{ℓ', m'}
     abdprime.sigma = spinsfast.map2salm(fprime_of_timeprime_directionprime[5], 2, output_ell_max)
+    
+    print(abdprime.psi0)
 
     return abdprime
