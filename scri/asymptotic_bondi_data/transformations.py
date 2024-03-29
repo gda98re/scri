f142d3292c2c9357603489e8a6a3392ac998ad6f
Guido Da Re <gdare@caltech.edu>
2024-03-29 11:24:44 -0700

    flags

--- scri/asymptotic_bondi_data/transformations.py
+++ scri/asymptotic_bondi_data/transformations.py
@@ -304,7 +304,7 @@ def transform(self, **kwargs):
                                                sf.Grid(self.psi3.evaluate(grid_rotors), spin_weight=-1),
                                                sf.Grid(self.psi4.evaluate(grid_rotors), spin_weight=-2),
                                                sf.Grid(self.sigma.evaluate(grid_rotors), spin_weight=2)]), axis=1)
-    
+
     # Make this into a Modes object, so it can keep track of its spin weight, etc., through the
     # various operations needed below.
     supertranslation = sf.Modes(supertranslation, spin_weight=0).real
@@ -334,9 +334,15 @@ def transform(self, **kwargs):
     latest_complete_timeprime = np.min(timeprime_of_finaltime_directionprime.view(np.ndarray))
     timeprime = timeprime[(timeprime >= earliest_complete_timeprime) & (timeprime <= latest_complete_timeprime)]
     
+    print('interpolant data psi0: ', sf.Grid(self.psi0.evaluate(grid_rotors), spin_weight=2))
+    print('interpolant data psi1: ', sf.Grid(self.psi1.evaluate(grid_rotors), spin_weight=1))
+    print('interpolant data psi2: ', sf.Grid(self.psi2.evaluate(grid_rotors), spin_weight=0))
+    print('interpolant data psi3: ', sf.Grid(self.psi3.evaluate(grid_rotors), spin_weight=-1))
+    print('interpolant data psi4: ', sf.Grid(self.psi4.evaluate(grid_rotors), spin_weight=-2))
+    print('interpolant data sigma', sf.Grid(self.sigma.evaluate(grid_rotors), spin_weight=2))
+    
     # This will store the values of f(u', θ, ϕ) for the various functions `f`
-    f_of_timeprime = self.interpolant(timeprime)
-    print('timeprime', timeprime)
+    f_of_timeprime = self.interpolant(timeprime) #this is the problematic line timeprime is correct, the interpolant is problematic
     print('f_of_timeprime:', f_of_timeprime)
                 
     # ðu'(u', θ', ϕ') exp(iλ) / k(θ', ϕ')
@@ -422,5 +428,7 @@ def transform(self, **kwargs):
     abdprime.psi4 = spinsfast.map2salm(fprime_of_timeprime_directionprime[4], -2, output_ell_max)
     # σ'(u')_{ℓ', m'}
     abdprime.sigma = spinsfast.map2salm(fprime_of_timeprime_directionprime[5], 2, output_ell_max)
+    
+    print('psi0',abdprime.psi0 )
 
     return abdprime
